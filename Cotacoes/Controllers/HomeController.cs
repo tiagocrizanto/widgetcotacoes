2dc224608ddfba876ba5b581433f47b853bd6d07
Tiago Crizanto <tiagocrizanto@gmail.com>
2016-04-10 23:02:54 -0300

    Projeto asp net widget cotação de ações / dólar

---
+++ Cotacoes/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cotacoes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult GetCotacoes()
+        {
+            string json_data = "";
+            using (var w = new WebClient())
+            {
+                try
+                {
+                    IWebProxy defaultWebProxy = WebRequest.DefaultWebProxy;
+                    defaultWebProxy.Credentials = CredentialCache.DefaultCredentials;
+                    w.Proxy = defaultWebProxy;
+                    json_data = w.DownloadString("http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.PETR4&quotes=sp.ITUB4&quotes=sp.PCAR4&quotes=sp.AGRO3&quotes=BR.IBOVESPA&quotes=US.DOLARC");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("", ex);
+                }
+            }
+            return Json(json_data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Cotacoes/Scripts/Cotacoes/Cotacoes.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="../../jquery-1.10.0.js" />
+/// <reference path="../intranet.mobile.footer.js" />
+
+$(function() {
+    $.ajax({
+        type: 'GET',
+        cache: false,
+        url: '/Home/GetCotacoes/',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8"
+    })
+    .then(function (data) {
+
+        var json = $.parseJSON(data);
+
+        $.each(json, function (i, item) {
+            var tr = $("<tr>");
+            var display = $("<td>");
+            $(display).html(item.display);
+
+            var valor = $("<td>");
+            $(valor).html(item.valor);
+            
+            var img = createImage(parseFloat(item.variacao_percentual).toFixed(2));
+            var variacao = $("<td>").append(img);
+
+            $(tr).append(display);
+            $(tr).append(valor);
+            $(tr).append(variacao);
+
+            $("#cotacoes").append(tr);
+            
+        });
+    })
+    .fail(function (e) {
+        //Em caso de erro
+    });
+});
+
+function createImage(stats) {
+    var img;
+    if (stats > 0)
+        img = $("<img>").attr("src", "/Content/Images/setinha_up.gif");
+    else if (stats < 0)
+        img = $("<img>").attr("src", "/Content/Images/setinha_down.gif");
+
+    return img;
+}
